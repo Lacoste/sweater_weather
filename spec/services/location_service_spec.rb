eb05a56ba242cdf15fec9132cb929491d50348ea
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-31 11:23:57 -0600

    Includes simplecov and webmocks

---
+++ spec/services/location_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'LocationService', type: :service do
+  describe 'API call' do
+    it 'should successfully gather information from an API call' do
+
+      location = ""
+      response = LocationService.new(location)
+
+    end
+  end
+end
