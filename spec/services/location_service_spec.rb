735a111904361cb6d3ddbfb104decb9a4161d290
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-31 11:47:36 -0600

    Adds initital assertions to LocationService spec

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -4,9 +4,13 @@ describe 'LocationService', type: :service do
   describe 'API call' do
     it 'should successfully gather information from an API call' do
 
-      location = ""
+      location = "denver,co"
       response = LocationService.new(location)
 
+      parsed = JSON.parse(response.body, symbolize_name: true)
+
+      expect(parsed[:status]).to eq("OK")
+      # expect(parsed[:results])
     end
   end
 end
