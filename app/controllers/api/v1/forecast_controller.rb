7d4b1801f15de2815d26622962017f5bad87ff76
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-01 20:31:39 -0600

    Updates forecast#index, rendeing JSON matching mock view

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,15 +1,14 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
-    weather_data = DarkSkyService.new(location.lat, location.long).full_info
-    render json: WeatherDataSerializer.new(weather_data, location.details).to_hash
+    weather_data = DarkSkyService.new(location.latitude, location.longitude).full_info
+    render json: WeatherDataSerializer.new(weather_data, location).to_hash
   end
 
   private
 
   def location
-    # find or create by?
-    LocationService.new(params[:location])
+    @_city ||= Cities.find_or_create_city(params[:location])
   end
 
 end
