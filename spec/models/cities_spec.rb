7705f66ad13d7c0eaaa5423f2b74d185a19ec45d
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-02 07:08:14 -0600

    Adds find_or_create_city spec to Cities model

--- spec/models/cities_spec.rb
+++ spec/models/cities_spec.rb
@@ -9,5 +9,24 @@ describe Cities, type: :model do
     it {should validate_presence_of :state_abrev}
     it {should validate_presence_of :country}
   end
-  
+
+  describe 'Class Methods' do
+    context '::find_or_create_city()' do
+      it 'finds or creates a new city based on search input info' do
+        search_name = "denver,co"
+
+        expect(Cities.count).to eq(0)
+
+        Cities.find_or_create_city(search_name)
+
+        expect(Cities.count).to eq(1)
+        expect(Cities.first.name).to eq("Denver")
+        expect(Cities.first.state_abrev).to eq("CO")
+
+        Cities.find_or_create_city(search_name)
+        #finds existing city - no new city is added to table
+        expect(Cities.count).to eq(1)
+      end
+    end
+  end
 end
