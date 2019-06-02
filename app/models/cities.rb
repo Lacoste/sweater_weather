501acbaad7ac7657bf2658a3c60d35501ae0e77a
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-01 20:28:15 -0600

    Moves Cities.find_or_create_city method to Cities model

--- app/models/cities.rb
+++ app/models/cities.rb
@@ -4,5 +4,21 @@ class Cities < ApplicationRecord
                         :longitude,
                         :name,
                         :state_abrev,
-                        :country    
+                        :country
+
+
+  def self.find_or_create_city(location)
+    if Cities.find_by(search_name: location)
+      return Cities.find_by(search_name: location)
+    else
+      city_info = LocationService.new(location)
+      Cities.create(search_name: location,
+                    latitude: city_info.lat,
+                    longitude: city_info.long,
+                    name: city_info.details[0][:short_name],
+                    state_abrev: city_info.details[2][:short_name],
+                    country: city_info.details[3][:short_name])
+    end
+  end
+
 end
