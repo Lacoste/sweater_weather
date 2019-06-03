ffccfe6712e05f0c35b9048f8cfeda42e7072d9f
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-02 19:19:28 -0600

    Creates WeatherData PORO, cleans up formatting and logic in WeatherDataSerializer. Cleans up ForecastController as result.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,18 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
-    weather_data = DarkSkyService.new(location.latitude, location.longitude).full_info
-    render json: WeatherDataSerializer.new(weather_data, location).to_hash
+    weather_data = WeatherData.new(params[:location])
+    render json: WeatherDataSerializer.new(weather_data)
   end
-
-  private
-
-  def location
-    @_city ||= Cities.find_or_create_city(params[:location])
-    @_city.find_or_create_background
-    return @_city
-  end
-
-
 
 end
--- app/serializers/weather_data_serializer.rb
+++ app/serializers/weather_data_serializer.rb
@@ -1,11 +1,11 @@
 class WeatherDataSerializer
 
-  def initialize(weather_data, location)
-    @location = location
-    @currently = weather_data[:currently]
-    @today = weather_data[:daily][:data][0]
-    @hourly = weather_data[:hourly][:data][1..8]
-    @rest_of_week = weather_data[:daily][:data][1..5]
+  def initialize(weather_data)
+    @location = weather_data.location
+    @currently = weather_data.currently
+    @today = weather_data.today
+    @hourly = weather_data.hourly_formatted
+    @rest_of_week = weather_data.rest_of_week_formatted
   end
 
   def to_hash
@@ -20,7 +20,9 @@ class WeatherDataSerializer
           city_state: "#{@location.name}, #{@location.state_abrev}",
           country: @location.country,
           search_time: Time.at(@currently[:time]).strftime("%l:%M %p"),
-          date: Time.at(@currently[:time]).strftime("%m/%d")
+          date: Time.at(@currently[:time]).strftime("%m/%d"),
+          latitude: @location.latitude,
+          longitude: @location.longitude
         },
         details: {
           icon: @currently[:icon],
@@ -32,38 +34,11 @@ class WeatherDataSerializer
           summary: @today[:summary],
         },
         forecast: {
-          hourly: hourly_formatted(@hourly),
-          extended: rest_of_week_formatted(@rest_of_week)
+          hourly: @hourly,
+          extended: @rest_of_week
         }
       }
     }
   end
 
-  private
-
-  def hourly_formatted(hourly)
-    hourly.map do |hour|
-      hour_hash = {}
-      hour.each do |key, value|
-        hour_hash[:hour] = Time.at(hour[:time]).strftime("%l%p")
-        hour_hash[:temp] = hour[:temperature].round
-      end
-      hour_hash
-    end
-  end
-
-  def rest_of_week_formatted(rest_of_week)
-    rest_of_week.map do |day|
-      daily_hash = {}
-      day.each do |key, value|
-        daily_hash[:date] = Time.at(day[:time]).strftime("%A")
-        daily_hash[:icon] = day[:icon]
-        daily_hash[:chance_precip] = "#{(day[:precipProbability] * 100).round}%"
-        daily_hash[:precip_type] = day[:precipType]
-        daily_hash[:high] = day[:temperatureHigh].round
-        daily_hash[:low] = day[:temperatureLow].round
-      end
-      daily_hash
-    end
-  end
 end
