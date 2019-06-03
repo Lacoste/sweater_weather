46393df9a5b39e69c8e2fd2bac7679c61c59d8c2
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-03 17:29:39 -0600

    Creates Api/V1/Users controller and request spec.

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      user.generate_api_key
+      render status: 201, json: {
+        api_key: user.api_key
+      }
+    else
+      render status: 400, json: {
+        invalid: user.errors.full_messages.join(", ")
+      }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
+end
---
+++ spec/requests/api/v1/users/account_creation_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'POST /api/v1/users' do
+  context 'with valid information' do
+    it 'creates an account for a user, and provides an api_key in the response' do
+      params =  {
+        "email": "whatever@example.com",
+        "password": "password",
+        "password_confirmation": "password"
+        }
+
+      post '/api/v1/users', params: params
+
+      creation_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(201)
+      expect(creation_response[:api_key].present?).to eq(true)
+    end
+  end
+
+  context 'with invalid information' do
+    it 'returns an error message and a 404 code' do
+      user = create(:user, email: "whatever@example.com")
+
+      params =  {
+        "email": "whatever@example.com",
+        "password": "password",
+        "password_confirmation": "non-matching-pw"
+        }
+
+      post '/api/v1/users', params: params
+
+      error_response = JSON.parse(response.body, symbolize_names: true)
+      error_message = "Email has already been taken, Password confirmation doesn't match Password"
+
+      expect(response.status).to eq(400)
+      expect(error_response[:invalid]).to eq(error_message)
+    end
+  end
+end
