03739ccba48fbb3fda77ff38b6122ba805cf0cd5
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-03 19:17:58 -0600

    Changes a local variable in spec to be more clear and readable.

--- spec/requests/api/v1/users/account_creation_spec.rb
+++ spec/requests/api/v1/users/account_creation_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe 'POST /api/v1/users' do
   context 'with valid information' do
     it 'creates an account for a user, and provides an api_key in the response' do
-      params =  {
+      post_body =  {
         "email": "whatever@example.com",
         "password": "password",
         "password_confirmation": "password"
         }
 
-      post '/api/v1/users', params: params
+      post '/api/v1/users', params: post_body
 
       creation_response = JSON.parse(response.body, symbolize_names: true)
 
@@ -22,17 +22,17 @@ describe 'POST /api/v1/users' do
     it 'returns an error message and a 404 code' do
       user = create(:user, email: "whatever@example.com")
 
-      params =  {
+      post_body =  {
         "email": "whatever@example.com",
         "password": "password",
         "password_confirmation": "non-matching-pw"
         }
 
-      post '/api/v1/users', params: params
+      post '/api/v1/users', params: post_body
 
       error_response = JSON.parse(response.body, symbolize_names: true)
       error_message = "Email has already been taken, Password confirmation doesn't match Password"
-      
+
       expect(response.status).to eq(400)
       expect(error_response[:invalid]).to eq(error_message)
     end
