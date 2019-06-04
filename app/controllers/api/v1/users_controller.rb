dc1a53c12765dbb31ca3b3dc7beccca83d636182
Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
2019-06-03 18:39:53 -0600

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,13 +5,9 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       user.generate_api_key
-      render status: 201, json: {
-        api_key: user.api_key
-      }
+      render status: 201, json: { api_key: user.api_key }
     else
-      render status: 400, json: {
-        invalid: user.errors.full_messages.join(", ")
-      }
+      render status: 400, json: { invalid: user.errors.full_messages.join(", ") }
     end
   end
 
