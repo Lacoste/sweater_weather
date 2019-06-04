9e75638184d3a5860d7e2fca2ef43891ae49e6e8
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-03 19:54:37 -0600

    Adds Api/V1/Session controller, routes, and spec. User can log in with valid POST.

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::SessionsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def create
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(user_params[:password])
+      session[:user_id] = user.id
+      render json: { api_key: user.api_key}
+    else
+      render status: 400, json: { invalid: 'Invalid Credentials' }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+
+end
