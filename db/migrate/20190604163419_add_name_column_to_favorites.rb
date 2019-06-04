0778fc744fa7de848ed2db4e9d2303f4bfe4a900
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-04 10:55:00 -0600

    Adds :name column to the Favorites table.

---
+++ db/migrate/20190604163419_add_name_column_to_favorites.rb
@@ -0,0 +1,5 @@
+class AddNameColumnToFavorites < ActiveRecord::Migration[5.2]
+  def change
+    add_column :favorites, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_022353) do
+ActiveRecord::Schema.define(version: 2019_06_04_163419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_022353) do
     t.bigint "cities_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["cities_id"], name: "index_favorites_on_cities_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
