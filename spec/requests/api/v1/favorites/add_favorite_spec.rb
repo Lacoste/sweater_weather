6ec310e76ff4796b4ec263ad61d8b00652732f9f
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-04 10:59:08 -0600

    Adds sad path test to FavoritesController spec - Built out passing functionality in Controller.

--- spec/requests/api/v1/favorites/add_favorite_spec.rb
+++ spec/requests/api/v1/favorites/add_favorite_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe 'POST /api/v1/favorites' do
   context 'with a valid api_key' do
     it 'adds a favorite for the user, and returns a 201 status code' do
-      user = create(:user, api_key: "jgn983hy48thw9begh98h4539h4")
+      user = create(:user, api_key: "abc123")
       expect(user.favorites.count).to eq(0)
-      # If you decide to store cities in your database you can send an id if you prefer
+
       post_body = {
         "location": "Denver, CO",
-        "api_key": "jgn983hy48thw9begh98h4539h4"
+        "api_key": "abc123"
         }
 
       post '/api/v1/favorites', params: post_body
@@ -16,14 +16,28 @@ describe 'POST /api/v1/favorites' do
       favorite_response = JSON.parse(response.body, symbolize_names: true)
 
       expect(response.status).to eq(201)
-      expect(favorite_response[:outcome]).to eq("Successfully added")
+      expect(favorite_response[:outcome]).to eq("Successfully Added")
       expect(user.favorites.count).to eq(1)
     end
   end
 
   context 'with an invalid api_key' do
-    it 'no favorites are added, and a 400 status code is returned' do
+    it 'no favorites are added, and a 401 status code is returned' do
+      user = create(:user, api_key: "abc123")
+      expect(user.favorites.count).to eq(0)
 
+      post_body = {
+        "location": "Denver, CO",
+        "api_key": "xyz123"
+        }
+
+      post '/api/v1/favorites', params: post_body
+
+      favorite_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(401)
+      expect(favorite_response[:invalid]).to eq("Unauthorized")
+      expect(user.favorites.count).to eq(0)
     end
   end
 end
