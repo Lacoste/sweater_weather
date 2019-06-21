3ef34556d7a9c720d3e08ad10e3a761882c6a5cc
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-21 15:19:12 -0600

    Refactors Favorites Serializer, removes logic from Favorites Model.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,28 +1,4 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  belongs_to :cities
-
-  def self.formatted_weather_data
-    all_weather_data.map do |weather_data|
-      [weather_data.location, weather_data.currently, weather_data.today]
-    end
-  end
-
-  def self.all_weather_data
-    favorite_cities.map do |city|
-      WeatherData.new(city.search_name)
-    end
-  end
-
-  def self.favorite_cities
-    favorites = Favorite.all
-    favorites.map do |fav|
-      fav.find_city
-    end
-  end
-
-  def find_city
-    Cities.find(self.cities_id)
-  end
-
+  belongs_to :cities  
 end
